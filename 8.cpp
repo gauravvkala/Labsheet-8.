07bba547077110fa43b4df37ca67c57f32675ee9
gauravvkala <kalagaurav57@gmail.com>
2025-11-27 21:26:14 +0530

    function to find out the sum of diagonal numbers in matrix.

---
+++ 8.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int diagonalSum(int a[][10], int n) {
+    int sum = 0;
+    for(int i=0;i<n;i++)
+        sum += a[i][i];
+    return sum;
+}
+
+int main() {
+    int n;
+    cout << "Enter size of square matrix: ";
+    cin >> n;
+
+    int a[10][10];
+    cout << "Enter matrix:\n";
+    for(int i=0;i<n;i++)
+        for(int j=0;j<n;j++)
+            cin >> a[i][j];
+
+    cout << "Sum of diagonal elements = " << diagonalSum(a, n);
+    return 0;
+}
